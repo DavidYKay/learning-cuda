2c20a063b4a918436c1b5a16d91653d939d01860
David Young-chan Kay <davidykay@gmail.com>
2016-04-10 10:15:12 -0700

    completed Julia on-GPU implementation.

--- chapter4/julia/julia.cu
+++ chapter4/julia/julia.cu
@@ -7,17 +7,17 @@
 struct cuComplex {
   float r;
   float i;
-  cuComplex(float a, float b) : r(a), i(b) {}
-  float magnitude2(void) { return r * r + i * i; }
-  cuComplex operator*(const cuComplex &a) {
+  __device__ cuComplex(float a, float b) : r(a), i(b) {}
+  __device__ float magnitude2(void) { return r * r + i * i; }
+  __device__ cuComplex operator*(const cuComplex &a) {
       return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
   }
-  cuComplex operator+(const cuComplex &a) {
+  __device__ cuComplex operator+(const cuComplex &a) {
       return cuComplex(r+a.r, i+a.i);
   }
 };
 
-int julia (int x, int y) {
+__device__ int julia (int x, int y) {
   const float scale = 1.5;
   float jx = scale * (float)(DIM/2 - x) / (DIM/2);
   float jy = scale * (float)(DIM/2 - y) / (DIM/2);
@@ -25,7 +25,6 @@ int julia (int x, int y) {
   cuComplex c(-0.8, 0.156);
   cuComplex a(jx, jy);
 
-  //int i = 0;
   for (int i = 0; i<200; i++) {
     a = a * a + c;
     if (a.magnitude2() > 1000) {
@@ -36,25 +35,33 @@ int julia (int x, int y) {
   return 1;
 }
 
-void kernel(unsigned char *ptr) {
-  for (int y = 0; y < DIM; y++) {
-    for (int x = 0; x < DIM; x++) {
-      int offset = x + y * DIM;
+__global__ void kernel(unsigned char *ptr) {
 
-      int juliaValue = julia(x, y);
-      ptr[offset * 4 + 0] = 0;
-      ptr[offset * 4 + 1] = 255 * juliaValue;
-      ptr[offset * 4 + 2] = 0;
-      ptr[offset * 4 + 3] = 255;
-    }
-  }
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int offset = x + y * gridDim.x;
+
+    int juliaValue = julia(x, y);
+    ptr[offset * 4 + 0] = 0;
+    ptr[offset * 4 + 1] = 255 * juliaValue;
+    ptr[offset * 4 + 2] = 0;
+    ptr[offset * 4 + 3] = 255;
 }
 
 int main( void ) {
   CPUBitmap bitmap(DIM, DIM);
-  unsigned char *ptr = bitmap.get_ptr();
+  unsigned char *dev_bitmap;
 
-  kernel(ptr);
+  HANDLE_ERROR(cudaMalloc((void**) &dev_bitmap, bitmap.image_size()));
 
+  dim3 grid(DIM, DIM);
+
+  kernel<<<grid,1>>>(dev_bitmap);
+
+  HANDLE_ERROR(cudaMemcpy(bitmap.get_ptr(),
+              dev_bitmap,
+              bitmap.image_size(),
+              cudaMemcpyDeviceToHost));
   bitmap.display_and_exit();
+  cudaFree(dev_bitmap);
 }
